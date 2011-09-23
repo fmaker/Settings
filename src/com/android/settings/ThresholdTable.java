f420dfb08650697ac6b495b0a31b285d49d074ac
Frank Maker <flmaker@ece.ucdavis.edu>
2011-09-23 16:55:33 -0700

    Simple activity to test threshold code
    Change-Id: I92b3d6ce5be317f166e10526d988b51f53b1a6c8

--- src/com/android/settings/ThresholdTable.java
+++ src/com/android/settings/ThresholdTable.java
@@ -1,16 +1,33 @@
 package com.android.settings;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.content.Profile;
+import android.content.ThresholdTableGenerator;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 
 public class ThresholdTable extends Activity {
 	private static final String TAG = "ThresholdTable";
 
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.d(TAG, "onCreate()");
+
+		Log.d(TAG, "Creating Profile");
+		Profile p = new Profile(getApplicationContext());
+
+		Log.d(TAG, "Creating ThresholdTableGenerator");
+		File tmp = getDir("array.bin", MODE_PRIVATE);
+		Log.d(TAG, "tmp file = "+tmp);
+		ThresholdTableGenerator gen = new ThresholdTableGenerator(p,tmp);
+		gen.getThreshold();
+		
+		Log.d(TAG, "Got threshold table");
 	}
 
 }
